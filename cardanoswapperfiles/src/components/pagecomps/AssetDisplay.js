933124519612302ec5823c9d12e4fdff817983ce
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-07-16 02:24:34 -0700

    Changes

--- cardanoswapperfiles/src/components/pagecomps/AssetDisplay.js
+++ cardanoswapperfiles/src/components/pagecomps/AssetDisplay.js
@@ -1,24 +1,26 @@
 import React from "react";
 import './assetdisplay.css'
 import crypto from '../../assets/ada.png'
-import {
-    Link,
-} from "react-router-dom"
+
+const DisplayImage = (name) => {
+    if (name === "ADA") {
+        return (
+            <img src={crypto} alt="ada" width="25px" height="25px" />
+        )
+    }
+}
 
 export const AssetDisplay = (name, amount) => {
     return (
         <div className="AssetDisplayContainer">
             <div className="AssetImage">
-
+                {DisplayImage(name)}
             </div>
             <div className="AssetAmount">
                 {amount}
             </div>
             <div className="AssetName">
                 {name}
-            </div>
-            <div className="ada_logo">
-                <Link to="/"><img src={crypto} alt="ada" width="30px" height="30px" /></Link>
             </div>
         </div>
     )
