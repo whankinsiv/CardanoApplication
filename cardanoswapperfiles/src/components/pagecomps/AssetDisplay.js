02218b887c20c67c41f46c1073e24e255b0afc1e
bne88 <104248745+bne88@users.noreply.github.com>
2022-07-15 21:07:45 -0700

    Update AssetDisplay.js

--- cardanoswapperfiles/src/components/pagecomps/AssetDisplay.js
+++ cardanoswapperfiles/src/components/pagecomps/AssetDisplay.js
@@ -1,5 +1,10 @@
 import React from "react";
 import './assetdisplay.css'
+import crypto from '../../assets/ada.png'
+import {
+    Link,
+} from "react-router-dom"
+
 export const AssetDisplay = (name, amount) => {
     return (
         <div className="AssetDisplayContainer">
@@ -11,6 +16,9 @@ export const AssetDisplay = (name, amount) => {
             </div>
             <div className="AssetName">
                 {name}
+            </div>
+            <div className="ada_logo">
+                <Link to="/"><img src={crypto} alt="ada" width="30px" height="30px" /></Link>
             </div>
         </div>
     )
